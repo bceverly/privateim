fde5c41e46166c8036f950fcb488162e15726e5b
Bryan Everly <bceverly@Bryans-Macbook-Pro.local>
2015-03-22 17:53:11 -0400

    Added usage

--- src/privateim.c
+++ src/privateim.c
@@ -4,17 +4,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
-#define BUF_SIZE 8
+void print_usage() {
+	printf("usage:  privateim --ip IP_ADDRESS --port PORT_NUM\n");
+}
 
 int main(int argc, char *argv[]) {
-	char str_buf[BUF_SIZE + 1];
+	if (argc == 1) {
+		print_usage();
+		return -1;
+	}
 
-	printf("Enter string: ");
-	fgets(str_buf, sizeof str_buf, stdin);
-	trim_str(str_buf, BUF_SIZE);
+	if ((argc == 2) && (strcasecmp(argv[1], "--help") == 0)) {
+		print_usage();
+		return -1;
+	}
 
-	printf("The buffer is: %s\n", str_buf);
+	printf("Nothing to see here...\n");
+
 
 	return 0;
 }
